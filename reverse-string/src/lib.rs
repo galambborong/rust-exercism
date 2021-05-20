c6cda37d76a3c61d4309a42100819331afe84127
Peter Keenan <keenan@posteo.net>
2021-05-20 11:39:01 +0100

    Update to implicit return and add docstring

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,5 +1,6 @@
 use unicode_segmentation::UnicodeSegmentation;
 
+/// Reverses any input string
 pub fn reverse(input: &str) -> String {
-    return input.graphemes(true).rev().collect::<String>();
+    input.graphemes(true).rev().collect::<String>()
 }
