bc3604ee4fb96d1de8f8ed31f620cf1f06d199f0
Peter Keenan <keenan@posteo.net>
2021-05-21 10:04:33 +0100

    Basic logic before refactor

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,41 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        // let total_minutes: i32 = (hours * 60) + minutes;
+        let mut clock_hours = hours;
+        let mut clock_minutes = minutes;
+        let mut mins_into_hours = minutes;
+
+        if hours > 23 {
+            clock_hours = hours % 24;
+        }
+
+        if minutes > 59 {
+            mins_into_hours = minutes / 60;
+            clock_hours += mins_into_hours;
+            clock_minutes -= 60;
+        }
+
+        Clock {
+            hours: clock_hours,
+            minutes: clock_minutes,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
