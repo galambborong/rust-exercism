7c41e3bef37dc275def8dfe03a41b6253c412d97
Peter Keenan <keenan@posteo.net>
2021-05-22 18:31:50 +0100

    Initial solution, all tests passing

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut sound: String = String::from("");
+
+    if n % 3 == 0 {
+        sound.push_str("Pling");
+    }
+    if n % 5 == 0 {
+        sound.push_str("Plang");
+    }
+    if n % 7 == 0 {
+        sound.push_str("Plong");
+    };
+
+    if sound.len() == 0 {
+        return n.to_string();
+    } else {
+        return sound;
+    }
 }
