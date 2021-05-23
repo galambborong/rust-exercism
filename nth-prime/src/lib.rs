fe13e6e8696891ea9dcb40b101d3150c17d01b56
Peter Keenan <keenan@posteo.net>
2021-05-23 22:27:58 +0100

    Solution for nth prime

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,20 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut primes = Vec::new();
+    let mut i = 2;
+    while primes.len() < n as usize + 1 {
+        if is_prime(i) {
+            primes.push(i)
+        }
+        i += 1;
+    }
+    primes[n as usize]
+}
+
+fn is_prime(n: u32) -> bool {
+    for i in 2..n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    n != 1
 }
