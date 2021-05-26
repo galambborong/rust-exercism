699562a1c781cc8f88a5d2c799e0be166b0f1f90
Peter Keenan <keenan@posteo.net>
2021-05-26 09:49:01 +0100

    Proverb solution, all tests passing

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut proverb: String = String::from("");
+    let mut pointer = 0;
+    for i in list {
+        if list.len() > 1 && i != &list[list.len() - 1] {
+            proverb = format!(
+                "{0}\nFor want of a {1} the {2} was lost.",
+                proverb,
+                &list[pointer],
+                &list[pointer + 1],
+            );
+            pointer += 1;
+        } else {
+            proverb = format!("{0}\nAnd all for the want of a {1}.\n", proverb, &list[0]);
+        }
+    }
+    format!("{}", proverb.trim())
 }
