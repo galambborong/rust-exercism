1d32980eca08899ebf43a6cbdbbd7a241575a47e
Peter Keenan <keenan@posteo.net>
2021-05-26 10:45:11 +0100

    Remove unnecessary referencing

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -7,12 +7,12 @@ pub fn build_proverb(list: &[&str]) -> String {
             proverb = format!(
                 "{0}\nFor want of a {1} the {2} was lost.",
                 proverb,
-                &list[pointer],
-                &list[pointer + 1],
+                list[pointer],
+                list[pointer + 1],
             );
             pointer += 1;
         } else {
-            proverb = format!("{0}\nAnd all for the want of a {1}.\n", proverb, &list[0]);
+            proverb = format!("{0}\nAnd all for the want of a {1}.\n", proverb, list[0]);
         }
     }
 
