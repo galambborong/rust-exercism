29f980423582967f2e90a4a554dc617055e64ffa
Peter Keenan <keenan@posteo.net>
2021-05-27 12:30:05 +0100

    first solution for sum-of-multiples

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,10 +1,8 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let mut total_sum: u32 = 0;
     for i in 1..limit {
-        if factors
-            .iter()
-            .any(|x| if x > &0 { i % x == 0 } else { false })
-        {
+        if factors.iter().any(|x| x > &0 && i % x == 0) {
+            println!("{} <<< HELLO", i);
             total_sum += i;
         }
     }
