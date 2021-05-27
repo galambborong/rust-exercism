e779974777072cab3a505be282dc0f14d241bb94
Peter Keenan <keenan@posteo.net>
2021-05-27 12:33:20 +0100

    Refactor of sum-of-multiples

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -2,7 +2,6 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let mut total_sum: u32 = 0;
     for i in 1..limit {
         if factors.iter().any(|x| x > &0 && i % x == 0) {
-            println!("{} <<< HELLO", i);
             total_sum += i;
         }
     }
