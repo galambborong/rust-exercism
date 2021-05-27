89d01f924127731f4dc8596455d37d152e9b77f3
Peter Keenan <keenan@posteo.net>
2021-05-27 09:14:16 +0100

    Solution for difference of squares exercise, all tests passing

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,14 +1,27 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+    let mut missing_values: u32 = n;
+    let mut total: u32 = 0;
+
+    while missing_values > 0 {
+        total += missing_values;
+        missing_values -= 1;
+    }
+
+    total * total
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+    let mut missing_values: u32 = n;
+    let mut total: u32 = 0;
+
+    while missing_values > 0 {
+        total += missing_values * missing_values;
+        missing_values -= 1;
+    }
+
+    total
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+    square_of_sum(n) - sum_of_squares(n)
 }
