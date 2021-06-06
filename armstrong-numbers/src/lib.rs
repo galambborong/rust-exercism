20fd6b749fdf9ac99750ea68ca1a15cc0dc9f9bb
Peter Keenan <keenan@posteo.net>
2021-06-06 08:53:02 +0100

    First solution of armstrong numbers, all tests passing

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,19 +1,20 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let digits = num
+    let digits: Vec<u32> = num
         .to_string()
         .chars()
         .map(|x| x.to_digit(10).unwrap())
-        .collect::<Vec<_>>()
-        .into_iter();
+        .collect();
 
-    println!("{}", digits.len());
+    let mut sum_of_pows = 0;
+    let n = digits.len() as u32;
 
-    match digits.map(|x| check_pow(x, digits.len())).sum() {
-        0 => true,
-        _ => false,
+    for digit in digits {
+        sum_of_pows += check_pow(digit, n);
     }
+
+    sum_of_pows == num
 }
 
-fn check_pow(a: u32, b: usize) -> u32 {
-    u32::pow(a, b as u32)
+fn check_pow(a: u32, b: u32) -> u32 {
+    u32::pow(a, b)
 }
