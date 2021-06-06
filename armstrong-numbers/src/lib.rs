fe45590006be42d93b40e7e5e730b7c75166a637
Peter Keenan <keenan@posteo.net>
2021-06-06 09:04:13 +0100

    Refactored solution

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -5,16 +5,7 @@ pub fn is_armstrong_number(num: u32) -> bool {
         .map(|x| x.to_digit(10).unwrap())
         .collect();
 
-    let mut sum_of_pows = 0;
     let n = digits.len() as u32;
 
-    for digit in digits {
-        sum_of_pows += check_pow(digit, n);
-    }
-
-    sum_of_pows == num
-}
-
-fn check_pow(a: u32, b: u32) -> u32 {
-    u32::pow(a, b)
+    digits.iter().map(|x| u32::pow(*x, n)).sum::<u32>() == num
 }
