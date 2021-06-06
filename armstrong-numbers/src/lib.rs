ddcb81f5842f759f4eff3f872066618f3c808fe7
Peter Keenan <keenan@posteo.net>
2021-06-06 08:44:10 +0100

    Incomplete solution for armstrong

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let digits = num
+        .to_string()
+        .chars()
+        .map(|x| x.to_digit(10).unwrap())
+        .collect::<Vec<_>>()
+        .into_iter();
+
+    println!("{}", digits.len());
+
+    match digits.map(|x| check_pow(x, digits.len())).sum() {
+        0 => true,
+        _ => false,
+    }
+}
+
+fn check_pow(a: u32, b: usize) -> u32 {
+    u32::pow(a, b as u32)
 }
