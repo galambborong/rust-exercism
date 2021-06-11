652d5dca0e7bbfcf016eb872e555f3443093479f
Peter Keenan <keenan@posteo.net>
2021-06-11 14:10:29 +0100

    Commit before refactor

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -5,14 +5,29 @@ pub fn reply(message: &str) -> &str {
     let statement = Regex::new(r"\.$").unwrap();
     let end_with_whitespaces = Regex::new(r".*\?$").unwrap();
     let shouting = Regex::new(r"[A-Z]+.*$").unwrap();
-    match (
-        statement.is_match(message),
-        end_with_whitespaces.is_match(message.trim()),
-        shouting.is_match(message),
-    ) {
-        (true, false, false) => "Whatever.",
-        (false, true, false) => "Sure.",
-        (false, false, true) => "Whoa, chill out!",
-        _ => "Not here",
+
+    if statement.is_match(message) {
+        return "Whatever.";
+    };
+
+    if end_with_whitespaces.is_match(message) {
+        return "Sure.";
+    };
+
+    if shouting.is_match(message) {
+        return "Whoa, chill out!";
     }
+
+    message
+
+    // match (
+    //     statement.is_match(message),
+    //     end_with_whitespaces.is_match(message.trim()),
+    //     shouting.is_match(message),
+    // ) {
+    //     (true, false, false) => "Whatever.",
+    //     (false, true, false) => "Sure.",
+    //     (false, false, true) => "Whoa, chill out!",
+    //     _ => "Not here",
+    // }
 }
