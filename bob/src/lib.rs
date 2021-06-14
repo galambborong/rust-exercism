ff2931997437bcd532964f9b1ddedfc905f671c1
Peter Keenan <keenan@posteo.net>
2021-06-14 10:34:13 +0100

    Add type annotations and change is_question to use char

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,10 +1,10 @@
 /// Provide Bob's correct response to questions and statements
 pub fn reply(message: &str) -> &str {
-    let trimmed_message = message.trim();
-    let is_question = trimmed_message.ends_with("?");
-    let is_shouting = trimmed_message.find(|x: char| x.is_uppercase()).is_some()
+    let trimmed_message: &str = message.trim();
+    let is_question: bool = trimmed_message.ends_with('?');
+    let is_shouting: bool = trimmed_message.find(|x: char| x.is_uppercase()).is_some()
         && trimmed_message == trimmed_message.to_uppercase();
-    let is_empty = trimmed_message.is_empty();
+    let is_empty: bool = trimmed_message.is_empty();
 
     match (is_empty, is_question, is_shouting) {
         (true, false, false) => "Fine. Be that way!",
