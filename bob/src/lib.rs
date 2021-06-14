6b04a78bf4511a6056abce60c3beb665d9cdc631
Peter Keenan <keenan@posteo.net>
2021-06-14 10:30:43 +0100

    Solution with match statements

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,42 +1,15 @@
-use regex::Regex;
-
 pub fn reply(message: &str) -> &str {
-    // println!("::{}::", message.trim());
+    let trimmed_message = message.trim();
+    let is_question = trimmed_message.ends_with("?");
+    let is_shouting = trimmed_message.find(|x: char| x.is_uppercase()).is_some()
+        && trimmed_message == trimmed_message.to_uppercase();
+    let is_empty = trimmed_message.is_empty();
 
-    // let statement = Regex::new(r"\.$").unwrap();
-    // let question = Regex::new(r".*\?$").unwrap();
-    let numbers = Regex::new(r"[^a-zA-Z]").unwrap();
-    // let shouting = Regex::new(r"[A-Z]+").unwrap();
-    // let shouting_question = Regex::new(r".*[A-Z]+\?$").unwrap();
-    if message.trim() == "" {
-        return "Fine. Be that way!";
-    };
-
-    // if message.trim().ends_with(".") {
-    //     return "Whatever.";
-    // };
-
-    if message.trim().ends_with("?") {
-        return "Sure.";
-    };
-
-    // if shouting.is_match(message) {
-    if message.trim().to_uppercase() == message.trim() {
-        println!(
-            "HELLO FROM IN HERE\n{}\n{}",
-            message,
-            message.to_uppercase()
-        );
-        if message.trim().ends_with("?") {
-            return "Calm down, I know what I'm doing!";
-        } else {
-            return "Whoa, chill out!";
-        };
-    };
-
-    // if numbers.is_match(message) {
-    //     return "Whatever";
-    // }
-
-    "Whatever."
+    match (is_empty, is_question, is_shouting) {
+        (true, false, false) => "Fine. Be that way!",
+        (false, true, false) => "Sure.",
+        (false, false, true) => "Whoa, chill out!",
+        (false, true, true) => "Calm down, I know what I'm doing!",
+        _ => "Whatever.",
+    }
 }
