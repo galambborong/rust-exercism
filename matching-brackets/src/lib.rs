5a2f747147e1c90aac7ec423a0f89520843cb51d
Peter Keenan <keenan@posteo.net>
2021-06-18 17:39:18 +0100

    Minor refactor of naming and scoping

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -2,23 +2,20 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
     let chars: Vec<&str> = string.split("").filter(|x| !x.trim().is_empty()).collect();
 
-    let openings: Vec<&str> = vec!["[", "(", "{"];
-    let closings: Vec<&str> = vec!["]", ")", "}"];
-
-    let pairs: Vec<Vec<&str>> = vec![openings, closings];
-
     match chars.len() {
         x if x == 0 => return true,
         x if x % 2 != 0 => return false,
-        _ => return check_pairs(chars, pairs),
+        _ => return check_pairs(chars),
     }
 }
 
-fn check_pairs(brackets: Vec<&str>, collection: Vec<Vec<&str>>) -> bool {
+fn check_pairs(brackets: Vec<&str>) -> bool {
+    let openings: Vec<&str> = vec!["[", "(", "{"];
+    let closings: Vec<&str> = vec!["]", ")", "}"];
     let mut truthy: bool = false;
     let mut bracket_locations: Vec<(usize, usize)> = Vec::new();
     for (brackets_idx, bracket) in brackets.iter().enumerate() {
-        for (openings_idx, opening_bracket) in collection[0].iter().enumerate() {
+        for (openings_idx, opening_bracket) in openings.iter().enumerate() {
             if bracket == opening_bracket {
                 bracket_locations.push((brackets_idx, openings_idx));
             }
@@ -43,7 +40,7 @@ fn check_pairs(brackets: Vec<&str>, collection: Vec<Vec<&str>>) -> bool {
 
     for n in idx..brackets.len() {
         println!("{} <-", brackets[n]);
-        if brackets[n] == collection[1][bracket_locations[idx - 1].1] {
+        if brackets[n] == closings[bracket_locations[idx - 1].1] {
             println!("Inside if on line 33. idx = {}", idx);
             truthy = true;
             idx -= 1;
