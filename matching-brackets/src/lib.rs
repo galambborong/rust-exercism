dffc7add1d2b6d129285e4c26b1507064bb9df4d
Peter Keenan <keenan@posteo.net>
2021-08-06 07:40:50 +0100

    Map out the mutable borrowing better

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,23 +1,23 @@
 /// Check brackets are correctly matched and balanced
 pub fn brackets_are_balanced(string: &str) -> bool {
     println!("1 - Unedited string: {}", string);
-    let chars: Vec<&str> = string.split("").filter(|x| !x.trim().is_empty()).collect();
+    let mut chars: Vec<&str> = string.split("").filter(|x| !x.trim().is_empty()).collect();
     println!("2 - Handled brackets: {:?}", chars);
 
     match chars.len() {
         x if x == 0 => return true,
         x if x % 2 != 0 => return false,
-        _ => return check_pairs(chars),
+        _ => return check_pairs(&mut chars),
     }
 }
 
-fn check_pairs(brackets: Vec<&str>) -> bool {
+fn check_pairs(brackets: &mut Vec<&str>) -> bool {
     let remaining_brackets = remove_matches(brackets);
     println!("3 - Vec after matches removed: {:?}", remaining_brackets);
     remaining_brackets.is_empty()
 }
 
-fn remove_matches(mut brackets: Vec<&str>) -> Vec<&str> {
+fn remove_matches(brackets: &mut Vec<&str>) -> &mut Vec<&str> {
     let matching_brackets: Vec<&str> = vec!["{}", "()", "[]"];
     for (idx, bracket) in brackets.iter().enumerate().step_by(2) {
         let temporary_pair = [bracket, brackets[idx + 1]].concat();
@@ -29,5 +29,5 @@ fn remove_matches(mut brackets: Vec<&str>) -> Vec<&str> {
         }
     }
 
-    brackets
+    &mut brackets
 }
